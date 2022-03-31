2459a9db30187e8396cc3f75a30811e4559f3615
unknown <qquangmminh19999@gmail.com>
2022-03-31 14:30:38 +0700

    th2

---
+++ src/com/codegym/AbsoluteNumberCalculator.java
@@ -0,0 +1,13 @@
+package com.codegym;
+
+public class AbsoluteNumberCalculator {
+    public static int findAbsolute(int number){
+        return 0;
+    }
+    public static int findAbsolute1(int number){
+        return 1;
+    }
+    public static int findAbsolute2(int number){
+        return 1;
+    }
+}
---
+++ test/com/codegym/AbsoluteNumberCalculatorTest.java
@@ -0,0 +1,34 @@
+package com.codegym;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AbsoluteNumberCalculatorTest {
+
+    @org.junit.jupiter.api.Test
+    void testFindAbsolute() {
+        int number = 0;
+        int expected = 0;
+        int result = AbsoluteNumberCalculator.findAbsolute(number);
+        assertEquals(expected,result);
+    }
+
+    @org.junit.jupiter.api.Test
+    void testFindAbsolute1() {
+        int number = 1;
+        int expected = 1;
+
+        int result = AbsoluteNumberCalculator.findAbsolute1(number);
+        assertEquals(expected, result);
+    }
+
+    @org.junit.jupiter.api.Test
+    void testFindAbsoluteNegative1() {
+        int number = -1;
+        int expected = 1;
+
+        int result = AbsoluteNumberCalculator.findAbsolute2(number);
+        assertEquals(expected, result);
+    }
+}
